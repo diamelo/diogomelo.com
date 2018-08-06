64016a7c8e727ec7ee7d36d587d30f0b1a2bb8b9
Diogo Melo <diogo.melo@edirectinsure.com>
2018-08-06 12:11:20 +0100

    fuel card via contentful

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Card.css'
 
-const Card = props => (
-    <div className="Card">
-        <img src={props.image} />
-        <h3>{props.title}</h3>
-        <p>{props.text}</p>
+const Card = ({ data }) => (
+    <div className="CardGroup">
+        {data.allContentfulProjects.edges.map(edge => 
+        <div className="Card">
+            <img src={edge.node.image.file.url} />
+            <h3>{edge.node.title}</h3>
+            <p>{edge.node.text}</p>
+         </div> )
+        }
     </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ const SectionCellGroup = styled.div`
   }
 `
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
@@ -46,24 +46,7 @@ const IndexPage = () => (
     </div>
     <div className="Cards">
       <h2>11 courses, more coming</h2>
-      <div className="CardGroup">
-        <Card
-          title="Design System"
-          text="12 sections"
-          image={require('../images/wallpaper.jpg')} />
-        <Card
-          title="React for Designers"
-          text="12 sections"
-          image={require('../images/wallpaper2.jpg')} />
-        <Card
-          title="Sound Design"
-          text="12 sections"
-          image={require('../images/wallpaper3.jpg')} />
-        <Card
-          title="ARKit 2"
-          text="12 sections"
-          image={require('../images/wallpaper4.jpg')} />
-      </div>
+      <Card data={data} />
     </div>
     <Section
       image={require('../images/wallpaper2.jpg')}
@@ -84,3 +67,21 @@ const IndexPage = () => (
 )
 
 export default IndexPage
+
+export const query = graphql`
+  query SiteProjectsQuery {
+    allContentfulProjects {
+    edges {
+      node {
+        image {
+          file {
+            url
+          }
+          }
+        title
+        text
+      }
+    }
+  }
+  }
+`
